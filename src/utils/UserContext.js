2d54d8d0f2d28356f15ae91635f2c3957338be0d
KIIT <kiit>
2025-01-15 01:39:52 +0530

    .

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,7 +1,8 @@
 import { createContext } from "react";
 
-const UserContext= createContext({
+const userContext= createContext({
     loggedInUser:"Default user",
 })
 
-export default UserContext;
+export default userContext;
+
